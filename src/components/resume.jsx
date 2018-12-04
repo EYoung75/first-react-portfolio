1d0849ed47c354aa45839ca01c55f3455eae7403
EYoung75 <eyoung75.ey@gmail.com>
2018-12-04 16:42:49 -0700

    updated resume component with applicable link

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -4,7 +4,7 @@ class Resume extends React.Component {
     render() {
         return (
             <div className="resume">
-                <iframe src="https://resume.creddle.io/embed/i1oe7do7liw"
+                <iframe src="https://resume.creddle.io/resume/i1oe7do7liw"
                 width="850" height="1000" seamless></iframe>
             </div>
         )
