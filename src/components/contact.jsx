3ebd8674e2c50c39296c7370321fb1c628339912
EYoung75 <eyoung75.ey@gmail.com>
2018-12-31 15:10:50 -0700

    added icons to contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import { FaGithub, FaLinkedin, FaEnvelopeOpenText, FaMobile } from "react-icons/fa";
 
 const Contact = () => {
     return (
@@ -7,10 +8,10 @@ const Contact = () => {
             <div>
                 <h1 className="projectHead">Points of Contact:</h1>
                 <div className="contact">
-                    <a href="https://www.linkedin.com/in/evanyoung4/">•LinkedIn: linkedin.com/in/evanyoung4/</a>
-                    <a href="https://github.com/EYoung75">•GitHub: github.com/EYoung75</a>
-                    <a href="mailto:Young.EvanJ@gmail.com">•Email: Young.EvanJ@gmail.com</a>
-                    <a href="blank">•Phone: (720)224-6718</a>
+                    <a href="https://www.linkedin.com/in/evanyoung4/"><FaLinkedin/>LinkedIn: linkedin.com/in/evanyoung4/</a>
+                    <a href="https://github.com/EYoung75"><FaGithub/>GitHub: github.com/EYoung75</a>
+                    <a href="mailto:Young.EvanJ@gmail.com"><FaEnvelopeOpenText/>Email: Young.EvanJ@gmail.com</a>
+                    <a href="blank"><FaMobile/>Phone: (720)224-6718</a>
                 </div>
             </div>
         </div>
