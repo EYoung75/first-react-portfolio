a159d5347d03443e209149a8fe8414c7fbe5bcc9
EYoung75 <eyoung75.ey@gmail.com>
2019-01-09 19:06:36 -0700

    enhancing mobile styling, added download resume button, still need to render appropriate mobile navbar

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Resume = () => {
     return (
         <div className="resume">
+            <a href="./src/Creddle _ Resume.pdf" download><button>Download Resume</button></a>
             <iframe src="https://resume.creddle.io/embed/i1oe7do7liw"
                 width="850" height="1000" seamless></iframe>
         </div>
